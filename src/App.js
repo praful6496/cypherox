f78afcf6060ce270f97ac1f4b7814fb448585c04
Praful ayar <105041890+praful6496@users.noreply.github.com>
2023-07-25 13:46:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import TodoList from './components/TodoList';
 import TaskForm from './components/TaskForm';
-import Navbar from 'react-bootstrap/Navbar';
+import { Button, Container, Navbar } from 'react-bootstrap';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [selectAll, setSelectAll] = useState(false);
 
   const handleAddTask = (description) => {
     const newTask = {
@@ -37,18 +38,42 @@ function App() {
     );
   };
 
+  const handleSelectAll = () => {
+    setSelectAll(!selectAll);
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => ({ ...task, completed: !selectAll }))
+    );
+  };
+
+  const handleDeleteSelected = () => {
+    setTasks((prevTasks) => prevTasks.filter((task) => !task.completed));
+  };
+
   return (
     <>
       <Navbar className="header-name" bg="dark" data-bs-theme="dark">
         <Navbar.Brand>Todo List App</Navbar.Brand>
       </Navbar>
-      <TaskForm onAddTask={handleAddTask} />
-      <TodoList
-        tasks={tasks}
-        onToggleComplete={handleToggleComplete}
-        onDeleteTask={handleDeleteTask}
-        onEditTask={handleEditTask}
-      />
+      <Container>
+        <TaskForm onAddTask={handleAddTask} />
+        <TodoList
+          tasks={tasks}
+          onToggleComplete={handleToggleComplete}
+          onDeleteTask={handleDeleteTask}
+          onEditTask={handleEditTask}
+        />
+        <div className="action-buttons">
+          <Button variant="danger" className="me-2" onClick={handleDeleteSelected}>
+            Delete Selected
+          </Button>
+          <Button variant="primary" className="me-2" onClick={handleSelectAll}>
+            {selectAll ? 'Unselect All' : 'Select All'}
+          </Button>
+          <Button variant="success" onClick={() => setTasks([])}>
+            Delete All
+          </Button>
+        </div>
+      </Container>
     </>
   );
 }
